eb56fac6a6fc5e5286cf42faff0c95418904449d
Shivam Patel <shivampatel3301@gmail.com>
2025-05-11 11:26:12 -0400

    init commit

--- src/main/java/MySQL.java
+++ src/main/java/MySQL.java
@@ -21,7 +21,7 @@ public class MySQL {
 
 	private String dburl = "jdbc:mysql://localhost:3306/project";
 	private String dbuname = "root";
-	private String dbpassword = "password";
+	private String dbpassword = "rootrootroot";
 	private String dbdriver = "com.mysql.cj.jdbc.Driver";
 
 	/*
